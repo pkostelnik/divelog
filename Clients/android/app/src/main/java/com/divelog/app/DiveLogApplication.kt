0f7358cd68eaaac8000c4ba351d12e88c529c656
Pawel Kostelnik <pkostelnik@outlook.com>
2026-04-11 21:20:33 +0200

    Exclude native projects from repo to fix Azure Oryx Java detection
    Capacitor (ios/, android/) and native Clients are now gitignored.
    Recreate with: npx cap add ios && npx cap add android
    Local files are preserved.

--- Clients/android/app/src/main/java/com/divelog/app/DiveLogApplication.kt
+++
@@ -1,5 +0,0 @@
-package com.divelog.app
-
-import android.app.Application
-
-class DiveLogApplication : Application()
--- android/app/src/main/java/com/divelog/studio/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.divelog.studio;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
